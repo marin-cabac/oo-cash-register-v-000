f165e1a241c47fd19383ea10a12c15c494006176
Marin Cabac <marin.cabac@gmail.com>
2018-02-24 22:28:07 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+ 
+class CashRegister
+attr_accessor  :total, :qty, :items
 
+
+ #########################################
+def initialize(dsnt=0)
+  @total = 0
+  @dsnt=dsnt #discount
+  @trn=0     #transaction
+  @items =[]
+  @qty=qty
+end
+
+#########################################
+def   add_item(n,val,qty=1)
+  @trn=(val*qty)
+  self.total +=@trn
+     qty.times {@items << n}
+end
+#########################################
+def discount()  @dsnt end
+#########################################
+def apply_discount
+  v=@dsnt
+
+  d=(@total-=@total/100*v)
+  v == 0 ? ("There is no discount to apply.") : ("After the discount, the total comes to $#{d}.")
+end
+#########################################
+def  void_last_transaction() @total -= @trn  end
+#########################################
+end
